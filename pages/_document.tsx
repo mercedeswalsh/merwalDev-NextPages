4c8580d81e62759e006368bad5fa5a5262b98a96
mercedeswalsh <mercedes.scott@live.com>
2023-09-13 11:51:36 -0500

    adding global styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+      </Head>
       <body className="bg-white text-black">
         <Main />
         <NextScript />
