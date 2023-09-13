128c7ee2d0f8e38431b5f334b70ffed8af9c9645
mercedeswalsh <mercedes.scott@live.com>
2023-09-13 12:16:25 -0500

    boop

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
-import { PortableText } from '@portabletext/react'
-import { useState } from 'react';
-import Link from 'next/link';
 import './Header.module.css'
+
+import { PortableText } from '@portabletext/react'
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +17,7 @@ export default function Header() {
 
           <div className="md:hidden">
             <button type="button" onClick={() => setIsOpen(!isOpen)}>
-              <svg viewBox="0 0 20 20" fill="currentColor">
+              <svg viewBox="0 0 20 20" fill="#000000">
                 {isOpen ? (
                   // This is an "X" icon
                   <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
